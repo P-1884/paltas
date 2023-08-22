64277d0efa200df5ec522e4a86d2ebd4c14fe037
Phil Holloway <54064261+P-1884@users.noreply.github.com>
2023-08-21 19:22:18 -0700

    Revert "Renaming folders to prevent filename conflict"
    This reverts commit f722090f25614a8e5b94cf3a0c0c6ac8ed1dc22c.

--- paltas/generate.py
+++ paltas/generate.py
@@ -62,10 +62,7 @@ def main():
 	try:
 		if not os.path.exists(args.save_folder):
 			os.makedirs(args.save_folder)
-	except Exception as ex:
-		print("EX here")
-		print(args.save_folder)
-		print(ex)
+	except:
 		pass
 	print("Save folder path: {:s}".format(args.save_folder))
 	# Copy out config dict
