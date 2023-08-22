e61a54b36e62cc01fd195f1dd4e2003680143d11
Phil Holloway <54064261+P-1884@users.noreply.github.com>
2023-08-21 19:50:10 -0700

    Bug fixes (fixing file directories)

--- paltas/Analysis/AnalysisConfigs/train_config_examp_tutorial.py
+++ paltas/Analysis/AnalysisConfigs/train_config_examp_tutorial.py
@@ -19,9 +19,9 @@ flip_pairs = None
 # Which terms to reweight
 weight_terms = None
 
-directory_to_save_model = './notebooks/End_to_End_Tutorial_Files/'
-directory_for_training_images =  './notebooks/End_to_End_Tutorial_Files/'
-directory_for_validation_images =  './notebooks/End_to_End_Tutorial_Files/'
+directory_to_save_model = '/home/runner/work/notebooks/End_to_End_Tutorial_Files/'
+directory_for_training_images =  '/home/runner/work/notebooks/End_to_End_Tutorial_Files/'
+directory_for_validation_images =  '/home/runner/work/notebooks/End_to_End_Tutorial_Files/'
 # The path to the folder containing the npy images for training
 npy_folders_train = glob(directory_for_training_images+'/training/*')
 npy_folders_train = [val for val in npy_folders_train if not val.endswith(".csv")]
@@ -29,7 +29,7 @@ npy_folders_train = [val for val in npy_folders_train if not val.endswith(".csv"
 tfr_train_paths = [os.path.join(path,'data.tfrecord') for path in npy_folders_train]
 metadata_paths_train = [os.path.join(path,'metadata.csv') for path in npy_folders_train]
 # The path to the folder containing the npy images for validation
-npy_folder_val = (directory_for_validation_images+'validation/1') #Assumes there is only one validation folder.
+npy_folder_val = (directory_for_validation_images+'validation/1/') #Assumes there is only one validation folder.
 # The path to the tf_record for the validation images
 tfr_val_path = os.path.join(npy_folder_val,'data.tfrecord')
 # The path to the training metadata
